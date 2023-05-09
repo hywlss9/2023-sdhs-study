073544c484907cb87523d39f01e7596f3e9ff914
HyeongJin Park <37681882+hywlss9@users.noreply.github.com>
2023-05-09 09:21:07 +0900

    FIX: router 수정

--- 0508/study-react/src/pages/router.jsx
+++ 0508/study-react/src/pages/router.jsx
@@ -1,12 +1,12 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Main from './main';
 
-function Router(){
+function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path='/' element={<Main />} />
       </Routes>
     </BrowserRouter>
   );
