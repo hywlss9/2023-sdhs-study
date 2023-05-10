8c81a9da8c947476cd3127bca09b121b61378600
HyeongJin Park <37681882+hywlss9@users.noreply.github.com>
2023-05-10 09:02:15 +0900

    FEAT: todo 생성 기능 추가

--- 0508/study-react/src/pages/todos/index.jsx
+++ 0508/study-react/src/pages/todos/index.jsx
@@ -1,12 +1,24 @@
+import { useState } from 'react';
+
 import * as S from './styled';
 
 import CreateItemBox from './CreateItemBox';
+import ItemList from './ItemList';
 
 function Todos() {
+  const [todoName, setTodoName] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const createTodo = () => {
+    setTodoName('');
+    setTodos(prevState => [...prevState, { id: prevState.length, name: todoName }]);
+  };
+
   return (
     <S.Container>
       <S.Title>To do list</S.Title>
-      <CreateItemBox />
+      <CreateItemBox value={todoName} createTodo={createTodo} onChange={setTodoName} />
+      <ItemList todos={todos} />
     </S.Container>
   );
 }
