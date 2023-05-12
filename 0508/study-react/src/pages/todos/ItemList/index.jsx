6fe7553cfdd0ada097541accd2022117e0e0e324
HyeongJin Park <37681882+hywlss9@users.noreply.github.com>
2023-05-12 10:18:03 +0900

    FEAT: 검색 기능 추가

--- 0508/study-react/src/pages/todos/ItemList/index.jsx
+++ 0508/study-react/src/pages/todos/ItemList/index.jsx
@@ -2,20 +2,33 @@ import * as S from './styled';
 
 import Todo from '../../../components/Todo';
 
-function ItemList({ todos, deleteTodo }) {
+function ItemList({ todos, searchValue, deleteTodo }) {
+  // 7. (6)에서 searchValue를 전달받았다.(검색창에 입력한 값)
+
   return (
     <S.ItemList>
-      {todos.map(({ id, name }) => {
-        const handleDeleteTodo = () => {
-          deleteTodo(id);
-        };
+      {todos
+        //todo.name = 1, 2, 3, 4, 5
+        // searchValue: 1
 
-        return (
-          <Todo key={id} deleteTodo={handleDeleteTodo}>
-            {name}
-          </Todo>
-        );
-      })}
+        // 8. filter 함수를 사용해준다
+        //    includes로 todo의 name에 검색한 값이(searchValue) 있는지 확인한다.
+        .filter(todo => {
+          console.log('name: ', todo.name, ' searchValue: ', searchValue);
+          return todo.name.includes(searchValue);
+        })
+        // [1]
+        .map(({ id, name }) => {
+          const handleDeleteTodo = () => {
+            deleteTodo(id);
+          };
+
+          return (
+            <Todo key={id} deleteTodo={handleDeleteTodo}>
+              {name}
+            </Todo>
+          );
+        })}
     </S.ItemList>
   );
 }
